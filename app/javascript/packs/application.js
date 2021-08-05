ca3f56f0ee6ebd7194362bfaab139a84712729f7
Mikey Couture <mcouture@deloitte.ca>
2021-08-05 16:35:57 -0400

    nav styling and functionality started

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,27 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// import Rails, { $ } from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+// require("semantic-ui-sass")
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+
+
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
+// //= require jquery
+// // = require semantic-ui
+
+
+// $(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("semantic-ui-sass")
+
+$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
